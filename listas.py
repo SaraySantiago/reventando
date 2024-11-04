bd4e984323b726242dc7f10ac1000c3e92a4d720
SaraySantiago <ssantiago18@iesrioarba.es>
2024-11-04 10:53:01 +0100

    .

--- listas.py
+++ listas.py
@@ -1,13 +1,16 @@
 #C:\Users\Admin\AppData\Local\Microsoft\WindowsApps\python.exe
 
 
-lista = list(range (10))
+lista = list(range (10)) #lista = list(range (1000000))
 print(lista)
 print(len(lista))
 
 for pos in range(len(lista)):
     print(pos)
-    lista[pos] += lista[pos-1]
+    if pos == 0: 
+        pass 
+    else: 
+      lista[pos] += lista[pos-1]
 
 print (lista)
     
