0a27fe3c2373b7d56a95065973c26f8fecd748d9
SaraySantiago <ssantiago18@iesrioarba.es>
2024-11-04 11:02:17 +0100

    .

---
+++ generator.demo.py
@@ -0,0 +1,9 @@
+#C:\Users\Admin\AppData\Local\Microsoft\WindowsApps\python.exe
+
+def generator(n):
+    yield value
+    value +=1 
+        
+if __name__ == "__main__": 
+    for item in generator(30):
+        print(item)
--- listas.py
+++ listas.py
@@ -1,16 +1,17 @@
 #C:\Users\Admin\AppData\Local\Microsoft\WindowsApps\python.exe
 
 
-lista = list(range (10)) #lista = list(range (1000000))
-print(lista)
-print(len(lista))
+#lista = list(range (10)) #lista = list(range (1000000))
+#print(lista)
+#print(len(lista))
 
-for pos in range(len(lista)):
-    print(pos)
-    if pos == 0: 
-        pass 
-    else: 
-      lista[pos] += lista[pos-1]
+#for pos in range(len(lista)):
+   # print(pos)
+  #  if pos == 0: 
+  #      pass 
+  #  else: 
+  #    lista[pos] += lista[pos-1]
 
-print (lista)
+##print (lista)
+    
     
